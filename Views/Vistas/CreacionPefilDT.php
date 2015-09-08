95f0a5954b5efa479c72c3836cca8df2de25b9f1
Alejo <alejok.024@gmail.com>
2015-09-07 22:46:37 -0500

    Creacion perfil tecnico iniciada

--- Views/Vistas/CreacionPefilDT.php
+++ Views/Vistas/CreacionPefilDT.php
@@ -33,68 +33,56 @@
             <div class="panel-heading">!Ya haces parte de SoccerFile ®!</FONT>
 
                 <div class="progress">
-                    <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%"> 40%
+                    <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width:30%"> 30%
                     </div>
                 </div>
 
                 <label>Estado creacion de pefil basico: En proceso</label>
-                
+
                 </div>
 
             <div class="jumbotron">
-                    <h3>¡Si que vas rapido!</h3>
-                    <p>Las personas necesitan saber que experiencia tienes y con que equipos has estado vinculado a lo largo de tu carrera profesional</p>
+                    <h3>Saludos amigo Director Tecnico!</h3>
+                    <p>Si estas acá es por que eres la persona que se encarga y se encargará de la dirección, instrucción y entrenamiento de un deportista individual o un conjunto de ellos para un equipo, te haremos unas preguntas con el fin de crear un perfil completo con tu informacion</p>
             </div>
             
     <div class="panel-body" > 
-
-    <form action="CreacionPefilJugador4.php" metod="POST" class="form-horizonal">
+        
+    <form action="CreacionPefilDT2.php" metod="POST" class="form">
                         <div class="form-group">
-                            <label for="Nickname">Nombre y/o apodo profesional</label>
-                            <input type="text" name="Nickname" id="Nickname" class="form-control" placeholder='"La Maravilla" "La Pulga"' required="required">
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label" for="Experience">Tiempo de experiencia</label>
-                            <select name="Experience" id="Experience" class="form-control" required>
+                            <label for="Name" class="control-label" >Nombre(s)</label>
+							<input type="text" name="Name" id="Name" class="form-control" placeholder="Nombre">
+						</div>
+						<div class="form-group">
+							<label for="Lastname" class="control-label" >Apellido</label>
+							<input type="text" name="Lastname" id="Lastname" class="form-control" placeholder="Apellido">
+						</div>
+						<div class="form-group">
+							<label for="Experience" class="control-label">Tiempo de experiencia</label>
+							<select name="Experience" id="Experience" class="form-control" required>
                                 <option value="">Seleccione</option>
-                                <option value="Ninguno">Nunca he jugado, pero se que puedo ser el mejor</option>
                                 <option value="6_meses">Menos de 6 meses</option>
                                 <option value="6_And_1">Entre 6 meses y 1 año</option>
                                 <option value="1_And_2">Entre 1 año y 2 años</option>
                                 <option value="2_And_3">Entre 2 y 3 años</option>
                                 <option value="3_And_4">Entre 3 y 4 años</option>
                                 <option value="4_And_5">Entre 4 y 5 años</option>
-                                <option value="5">Más de 5 años</option>
+                                <option value="5_And_10">Entre 5 y 10 años</option>
+                                <option value="10_And_15">Entre 10 y 15 años</option>
+                                <option value="15_And_20">Entre 15 y 20 años</option>
+                                <option value="20_And_30">Entre 20 y 30 años</option>
+                                <option value="30">Más de 30 años</option>
                             </select>
                         </div>
-                        <div class="form-group">
-                        <label for="MaritalStatus" class="">Estado Civil</label>
-                        <select name="MaritalStatus" id="MaritalStatus" class="form-control">
-                            <option value="">Seleccione</option>
-                            <option value="Single">Soltero</option>
-                            <option value="Married">Casado</option>
-                            <option value="Free_Union">Union Libre</option>
-                            <option value="Divorced">Divorciado</option>
-                            <option value="Widower">Viudo</option>
-                            <option value="Separate">Separado</option>
-                            <option value="Relationship">En una relacion</option>
-                            <option value="Other">Otro</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="CurrentOccupation" class="">Ocupacion actual</label>
-                        <input type="text" name="CurrentOccupation" id="CurrentOccupation" class="form-control" placeholder="Ingrese su ocupación actual" required="required">
-                    </div>
-
+						
                         <div class="form-group">  
                             <button type="submit" class="btn btn-block btn-soccer">Continuar</button>
+                        </div>
                         </div>
      </form>
             </div>
         </div>
     </div>
-</div>
-  
+</div>  
 </body>
 </html>
