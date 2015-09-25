a76dc3b3d96b38e0cb0dd0af83e39daf85fcc35b
Alejo <alejok.024@gmail.com>
2015-09-25 04:57:18 -0500

    Creacion perfil jugador

---
+++ Views/Vistas/PerfilEnd.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<head>
+
+
+  <link rel="stylesheet" href="../../Views/Styles/Bootstrap/css/bootstrap.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="../../Views/Styles/Bootstrap/js/bootstrap.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <link rel="icon" type="image/png" href="../../../Imagenes/ico.png"/>
+  <link rel="stylesheet" type="text/css" href="../../../SoccerFile/Views/Styles/Bootstrap/calendario/tcal.css" />
+  <script type="text/javascript" src="../../Views/Styles/Bootstrap/calendario/tcal.js"></script>
+  <title>Soccer File</title>
+</head>
+  
+</body>
+</html>
+<!--
+                    d8888   888          d8b           888     888 
+                   d88888   888          Y8P           888     888 
+                  d88P888   888                        888     888 
+                 d88P 888   888 .d88b.  8888 .d88b.    Y88b   d88P 
+                d88P  888   888d8P  Y8b "888d88""88b    Y88b d88P  
+               d88P   888   88888888888  888888  888     Y88o88P   
+              d8888888888   888Y8b.      888Y88..88P      Y888P    
+             d88P     888   888 "Y8888   888 "Y88P"        Y8P     
+                                         888                       
+                                        d88P                       
+                                      888P"                ____
+                                                       .~))>>
+                                                      .~)>>
+                                                    .~))))>>>
+                                                  .~))>>             ___
+                                                .~))>>)))>>      .-~))>>  
+                                              .~)))))>>       .-~))>>)>
+                                            .~)))>>))))>>  .-~)>>)>
+                        )                 .~))>>))))>>  .-~)))))>>)>
+                     ( )@@*)             //)>))))))  .-~))))>>)>
+                   ).@(@@               //))>>))) .-~))>>)))))>>)>
+                 (( @.@).              //))))) .-~)>>)))))>>)>
+               ))  )@@*.@@ )          //)>))) //))))))>>))))>>)>
+            ((  ((@@@.@@             |/))))) //)))))>>)))>>)>
+           )) @@*. )@@ )   (\_(\-\b  |))>)) //)))>>)))))))>>)>
+         (( @@@(.@(@ .    _/`-`  ~|b |>))) //)>>)))))))>>)>
+          )* @@@ )@*     (@) (@)  /\b|))) //))))))>>))))>>
+        (( @. )@( @ .   _/       /  \b)) //))>>)))))>>>_._
+         )@@ (@@*)@@.  (6,   6) / ^  \b)//))))))>>)))>>   ~~-.
+      ( @jgs@@. @@@.*@_ ~^~^~, /\  ^  \b/)>>))))>>      _.     `,
+       ((@@ @@@*.(@@ .   \^^^/' (  ^   \b)))>>        .'         `,
+        ((@@).*@@ )@ )    `-'   ((   ^  ~)_          /             `,
+          (@@. (@@ ).           (((   ^    `\        |               `.
+            (*.@*              / ((((        \        \      .         `.
+                              /   (((((  \    \    _.-~\     Y,         ;
+                             /   / (((((( \    \.-~   _.`" _.-~`,       ;
+                            /   /   `(((((()    )    (((((~      `,     ;
+                          _/  _/      `"""/   /'                  ;     ;
+                      _.-~_.-~           /  /'                _.-~   _.'
+                    ((((~~              / /'              _.-~ __._-~
+                                       ((((          __.-~ _.-~
+                                                   .'   .~~
+                                                   :_,'
+                                                   ~~~~~ -->
