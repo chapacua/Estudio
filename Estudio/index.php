34f4fdcf585d8206d1e9a6e9f6b20a5d3fd274e9
Alejo <alejok.024@gmail.com>
2015-09-25 05:06:10 -0500

    Validacion carga

---
+++ Estudio/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+
+
+    <title>FINANZAS </title>
+    
+    
+    <link rel="stylesheet" href="css/reset.css">
+
+    
+        <link rel="stylesheet" href="css/style.css">
+
+    
+    
+    
+  </head>
+
+  <body>
+    <form  class="contact_form" action="login.php" id="contact_form" method="post" runat="server">
+    <div class="wrap">
+        <div class="avatar">
+      <img src="imagenes/images.png">
+        </div>
+        <input type="text" placeholder="username" required name="txtusuario"  id = "txtusuario">
+        <div class="bar">
+            <i></i>
+        </div>
+        <input type="password" placeholder="password" required name="txtpassword" id = "txtpassword">
+        <button>Sign in</button>
+    </div>
+    
+        <script src="js/index.js"></script>
+
+    
+    
+    
+  </body>
+</html>
+<!--
+
+               _____________________________________________________
+               |                                                    |
+          __   |                                                    |
+   ______/  |  |                                                    |  
+  |-----    |  |                                                    |  
+  ||   |    |  |                                                    |   
+  ||___|    |  |                                                    |     
+  |   -     |  |____________________________________________________|
+  |]        |   |___|_=======____|_______|________|________|_____|
+ [__ ___/,-,\"-/,-,\  *******           // ,-,  ,-, \\ __#
+    \_/ ( 0 )||( 0 )  =======             ( 0 )( 0 )
+---------'-'----'-'------------------------'-'--'-'--------------------
+
+   ____   ____   ____       __       _____    ___
+  |  _ \ |  __| | ___\     /  \     |  ___|  / _ \
+  | |_|| | |__  | |       /    \    | |___  | | | |
+  |  __/ | ___| | |__    /  /\  \    ___  | | | | |
+  | |    | |__  | |  |  /  /__\  \  |     | | |_| |            
+  |_|    |____| |____/ /__/    \__\ |_____|  \___/
+  
+
+-->
